7b5f7847a94eebd28c10c31a082900d2a9e1198e
Richard Ricciardelli <ricciardelli2021@gmail.com>
2016-04-02 21:35:15 -0430

    :scissors: Removing fab and using ButterKnife for the views. Showing a Snackbar when feed is null

--- app/src/main/java/com/grability/coolestapps/activity/MainActivity.java
+++ app/src/main/java/com/grability/coolestapps/activity/MainActivity.java
@@ -17,33 +17,49 @@
 package com.grability.coolestapps.activity;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
 import com.grability.coolestapps.R;
+import com.grability.coolestapps.model.Feed;
+import com.grability.coolestapps.util.Constants;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final String LOG_TAG = getClass().getSimpleName();
+
+    @Bind(R.id.toolbar)
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        ButterKnife.bind(this);
+
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        Feed feed = (Feed) getIntent().getSerializableExtra(Constants.FEED_KEY);
+        if (feed == null) {
+            Snackbar.make(toolbar, R.string.error_connection, Snackbar.LENGTH_LONG)
+                    .setAction(R.string.action_retry, new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            Log.d(LOG_TAG, "Retry connection");
+                        }
+                    }).show();
+            // TODO Retrieve JSON from preferences
+        } else {
+            // TODO Render categories as tabs
+        }
     }
 
     @Override
