a05fd9ea5157f99da2eb7ca52255dacffc01616e
Richard Ricciardelli <ricciardelli2021@gmail.com>
2016-04-04 03:03:46 -0430

    :star: Adding detail activity

---
+++ app/src/main/java/com/grability/coolestapps/activity/DetailActivity.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright 2016 Richard Ricciardelli
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.grability.coolestapps.activity;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.MenuItem;
+
+import com.grability.coolestapps.R;
+import com.grability.coolestapps.fragment.DetailActivityFragment;
+import com.grability.coolestapps.model.Entry;
+import com.grability.coolestapps.util.Constants;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+/**
+ * Shows the details of an app.
+ *
+ * @author Richard Ricciardelli (ricciardelli2021@gmail.com)
+ * @version 1.0
+ */
+public class DetailActivity extends AppCompatActivity {
+
+    private final String LOG_TAG = getClass().getSimpleName();
+
+    @Bind(R.id.toolbar)
+    Toolbar mToolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+        ButterKnife.bind(this);
+        setSupportActionBar(mToolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        Entry entry = (Entry) getIntent().getSerializableExtra(Constants.ENTRY_KEY);
+        if (entry != null) {
+            Fragment fragment = new DetailActivityFragment();
+            Bundle args = new Bundle();
+            args.putSerializable(Constants.ENTRY_KEY, entry);
+            fragment.setArguments(args);
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment, fragment).commit();
+        } else {
+            Log.wtf(LOG_TAG, "This should not happen :: Entry is null");
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                break;
+            default:
+                Log.wtf(LOG_TAG, "No menu item");
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/grability/coolestapps/fragment/DetailActivityFragment.java
@@ -0,0 +1,105 @@
+/*
+ * Copyright 2016 Richard Ricciardelli
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.grability.coolestapps.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.grability.coolestapps.R;
+import com.grability.coolestapps.model.Entry;
+import com.grability.coolestapps.util.Constants;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class DetailActivityFragment extends Fragment {
+
+    private final String LOG_TAG = getClass().getSimpleName();
+
+    @Bind(R.id.logo)
+    ImageView logo;
+
+    @Bind(R.id.title)
+    TextView title;
+
+    @Bind(R.id.artist)
+    TextView artist;
+
+    @Bind(R.id.summary)
+    TextView summary;
+
+    @Bind(R.id.read_more)
+    Button readMore;
+
+    @Bind(R.id.collapse)
+    Button collapse;
+
+    public DetailActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_detail, container, false);
+        ButterKnife.bind(this, view);
+
+        Bundle args = getArguments();
+        Entry entry = (Entry) args.getSerializable(Constants.ENTRY_KEY);
+        if (entry != null) {
+            Glide.with(this).load(entry.getImage().get(2).getLabel()).into(logo);
+            title.setText(entry.getName().getLabel());
+            artist.setText(entry.getArtist().getLabel());
+            summary.setText(entry.getSummary().getLabel());
+        } else {
+            Log.e(LOG_TAG, "Entry is null");
+        }
+        return view;
+    }
+
+    @OnClick(R.id.read_more)
+    public void expand(View view) {
+        Log.d(LOG_TAG, "Expanding text");
+        summary.setMaxLines(Integer.MAX_VALUE);
+        summary.setEllipsize(null);
+        collapse.setVisibility(View.VISIBLE);
+        readMore.setVisibility(View.GONE);
+    }
+
+    @OnClick(R.id.collapse)
+    public void collapse(View view) {
+        Log.d(LOG_TAG, "Collapsing text");
+        // TODO Get this number from preferences
+        summary.setMaxLines(5);
+        summary.setEllipsize(TextUtils.TruncateAt.END);
+        collapse.setVisibility(View.GONE);
+        readMore.setVisibility(View.VISIBLE);
+    }
+
+}
